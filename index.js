feaab874628f7ced4fa532439f3cedac0524058c
Rahimov Umidjon <rahimovumidjon1402503@gmail.com>
2025-07-11 01:48:08 +0500

    responsive

--- index.js
+++ index.js
@@ -13,3 +13,71 @@ document.getElementById('handleServices').addEventListener('click', () => {
     }else    document.getElementById('rightBottom').style.transform = 'rotate(0)';
 });
 
+
+const navServices = document.getElementById('services-navbar')
+
+function handleServices() {
+    navServices.classList.toggle('hidden');
+}
+
+
+
+let count = 0;
+
+
+// document.getElementById('plus').addEventListener('click', () => {
+//     let  number = document.getElementById('number').value*1;
+//     number++;
+//     console.log(number)
+// })
+
+
+
+
+
+function incr() {
+    let  number = document.getElementById('number').value*1;
+    number++;
+    document.getElementById('number').value = number;
+
+}
+
+function decr() {
+    let  number = document.getElementById('number').value*1 ;
+    if (number > 0){
+        number--;
+        document.getElementById('number').value = number;
+    }
+}
+
+
+function calculator (){
+    const numberValues = document.getElementById('number').value;
+    const phoneValues = document.getElementById('tel').value;
+    const dataValues = document.getElementById('data').value;
+    const selectValues = document.getElementById('car').value;
+    const texturaValues = document.getElementById('textura').value;
+    const locationOneValues = document.getElementById('adres1').value;
+    const locationTwoValues = document.getElementById('adres2').value;
+
+
+    const btn = document.getElementById('btn-next');
+
+
+    if (numberValues && phoneValues && dataValues && selectValues &&  locationOneValues && locationTwoValues ){
+        btn.style.background = '#52AE32';
+        btn.style.color = 'white';
+        btn.innerHTML += 'за <small style="color:#FFE684; font-size: 16px "  >2 300 000 сум</small>'
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./node_modules/flowbite/**/*.js',"./index.html"],
+    content: ['./node_modules/flowbite/**/*.js', "./index.html", "./apartment/apartment.html", "./services/services.html", "./furniture/furniture.html" , "./services-two/services-two.html/furniture.html" , "./office/office.html","./calculator/calculator.html" ],
 
-  theme: {
-    extend: {
-      colors: {
-        'home' : '#52AE32',
-        'black-20': '#202020',
-        'black-80': '#808080',
-        'black-71': '#717171',
-        'gold': '#FFE684',
+    theme: {
+        extend: {
+            colors: {
+                'home': '#52AE32',
+                'black-20': '#202020',
+                'black-80': '#808080',
+                'black-71': '#717171',
+                'gold': '#FFE684',
 
-      },
-      boxShadow : {
-        'custom-card': '0px 0px 25px 5px #0000001A',
-        'custom-card-25': '0px 0px 20px 5px #0000001A',
-      },
-      fontFamily: {
-        inter: ['Inter', 'sans-serif'],
-      },
+            },
+            boxShadow: {
+                'custom-card': '0px 0px 25px 5px #0000001A',
+                'custom-card-25': '0px 0px 20px 5px #0000001A',
+            },
+            fontFamily: {
+                inter: ['Inter', 'sans-serif'],
+            },
+        },
     },
-  },
-  plugins: [
-    // require('flowbite/plugin')
-  ],
+    plugins: [
+        // require('flowbite/plugin')
+    ],
 }
