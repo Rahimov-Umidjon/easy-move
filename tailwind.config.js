ec68af103befa17eca403ddb77492fbb30bd1d1a
Rahimov Umidjon <rahimovumidjon1402503@gmail.com>
2025-07-14 19:21:52 +0500

    responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,12 +10,13 @@ export default {
                 'black-80': '#808080',
                 'black-71': '#717171',
                 'gold': '#FFE684',
-
             },
             boxShadow: {
                 'custom-card': '0px 0px 25px 5px #0000001A',
                 'custom-card-25': '0px 0px 20px 5px #0000001A',
-            },
+                'custom-card-20': '0px 20px 20px 10px #0000001A'
+
+},
             fontFamily: {
                 inter: ['Inter', 'sans-serif'],
             },
